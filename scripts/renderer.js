2f6bfde54110beb48cbc351d7a72819198cffbe5
Alois <AChristen@precitrame.com>
2022-10-26 17:19:43 +0200

    layout générale

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,21 +1,24 @@
 
 async function checkFolder(){
   let group = await server.sendGroup();
+  /*
   for(let i = 0; i < group.length; i++){
     let p = document.querySelector(`#group-${i}`)
     console.log(p)
     p.innerHTML = group[i]
   }
+  */
 }
 
 function updateTimer(timer){
     let seconds = (timer % 60).toString().padStart(2, '0');
     let minutes = ((timer - seconds) / 60).toString().padStart(2, '0');
     let text = `${minutes}:${seconds}`
-    document.querySelector('#timer').innerText = text;
+    document.querySelector('#timer-group').innerText = text;
 }
 
 let timer = null;
+let wakeUpTimers = {};
 let duration = 300;
 let currentTimer = duration;
 function startTimer(){
@@ -27,7 +30,7 @@ function startTimer(){
                 currentTimer = duration
                 checkFolder();
             }
-        }, 10)
+        }, 1000)
     }
 }
 
@@ -38,6 +41,30 @@ function pauseTimer(){
     }
 }
 
+function parseTime(time){
+    let now = new Date();
+    let hour = time.split(':')[0]
+    let minute = time.split(':')[1]
+    let parsedTime = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate(),
+        hour,
+        minute    
+    )
+    return parsedTime;
+}
+
+function startTimerAt(hour){
+    let time = parseTime(hour);
+    let delay = (time.getTime() - (new Date()).getTime());
+    console.log(hour, delay)
+    if(delay > 0){
+        clearTimeout(wakeUpTimers[hour]);
+        wakeUpTimers[hour] = setTimeout(startTimer, delay)
+    }
+}
+
 async function setup(){
     document.querySelector("#pause-btn").addEventListener("click", () => pauseTimer());
     document.querySelector("#start-btn").addEventListener("click", () => startTimer());
@@ -45,7 +72,9 @@ async function setup(){
     let config = await server.getConfig();
     duration = config['time_between_group']
     currentTimer = duration;
-    startTimer()
+    
+    startTimerAt(config.start_time);
+    startTimerAt(config.end_pause_time);
 }
 
 setup();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,1 @@
 
-function parseTime(time){
-    let now = new Date();
-    let hour = time.split(':')[0]
-    let minute = time.split(':')[1]
-    let parsedTime = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        hour,
-        minute    
-    )
-    return parsedTime;
-}
